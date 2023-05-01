2ca883333fb9229ad8fbad5f3d35a997c2051246
Alvaro <alvarofreire3@gmail.com>
2023-05-02 00:14:21 +0200

    implement code to animate the different parts of the same model

--- mapData.js
+++ mapData.js
@@ -20,9 +20,7 @@ export function createModel(data, scene, loader) {
             if (o.isMesh) {
                 o.material = materials[data.materialReference];
                 // Find the animation reference for this part of the model
-                const animationReference = data.partAnimations.find(
-                    (part) => part.partName === o.name
-                );
+                const animationReference = data.partAnimations.find((anim) => anim.partName.toLowerCase() === o.name.toLowerCase());
                 if (animationReference && animations[animationReference.animationReference]) {
                     // Assign the appropriate animation to this part
                     o.userData.animation = animations[animationReference.animationReference];
@@ -40,7 +38,6 @@ export function createModel(data, scene, loader) {
         data.instance = model;
     });
 }
-
 
 
 
