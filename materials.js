928685202992cf8731fee1fa7619ebd19dca045a
Alvaro <alvarofreire3@gmail.com>
2023-05-02 05:19:01 +0200

    test 2 animacion por partes

--- materials.js
+++ materials.js
@@ -1,39 +1,6 @@
 //materials.js:
 
-// Vertex shader
-const vertexShader = `
-    varying vec3 vNormal;
-    void main() 
-    { 
-        vNormal = normalize( normalMatrix * normal );
-        gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
-    }
-`;
-
-// Fragment shader
-const fragmentShader = `
-    varying vec3 vNormal;
-    uniform vec3 color1;
-    uniform vec3 color2;
-
-    void main() 
-    { 
-        float intensity = pow( vNormal.y, 4. );
-        vec3 color = mix( color1, color2, intensity );
-        gl_FragColor = vec4( color, 1. );
-    }
-`;
-
-// Building material
-const BUILDING_MATERIAL = new THREE.ShaderMaterial( {
-    uniforms: {
-        color1: { value: new THREE.Color("skyblue") }, // Change the color as needed
-        color2: { value: new THREE.Color("white") } // Change the color as needed
-    },
-    vertexShader: vertexShader,
-    fragmentShader: fragmentShader,
-    side: THREE.DoubleSide
-} );
+const MAP_OPACITY = 0.5;
 
 export const materials = {
     SPHERE_MATERIAL: new THREE.MeshPhongMaterial({color: 0xffff00}),
@@ -44,11 +11,11 @@ export const materials = {
         polygonOffsetFactor: 1,
         polygonOffsetUnits: 1
     }),
-    GRASS_MATERIAL: new THREE.MeshPhongMaterial({color: 0x006400, transparent: false}),
-    WATER_MATERIAL: new THREE.MeshPhongMaterial({color: 0x1E90FF, transparent: false}),
-    BUILDING_MATERIAL: BUILDING_MATERIAL
-    // Add more materials as needed
+    GRASS_MATERIAL: new THREE.MeshPhongMaterial({color: 0x006400, transparent: true, opacity: MAP_OPACITY}),
+    WATER_MATERIAL: new THREE.MeshPhongMaterial({color: 0xADD8E6, transparent: true, opacity: MAP_OPACITY}),
+    BUILDING_MATERIAL: new THREE.MeshPhongMaterial({color: 0x8A8AAA, transparent: true, opacity: MAP_OPACITY}),
+    RED: new THREE.MeshPhongMaterial({ color: 0xff0000 }),
+    BLUE: new THREE.MeshPhongMaterial({ color: 0x0000ff }),
 };
-
 
 
