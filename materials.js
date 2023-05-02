ab436527a5f0e4e6dae75ef023d1d87d87fce791
Alvaro <alvarofreire3@gmail.com>
2023-05-02 04:45:26 +0200

    test 2 animacion por partes

--- materials.js
+++ materials.js
@@ -3,12 +3,13 @@
 const MAP_OPACITY = 0.5;
 
 export const materials = {
-    SPHERE_MATERIAL: new THREE.MeshBasicMaterial({color: 0xffff00}),
-    ROAD_MATERIAL: new THREE.MeshBasicMaterial({color: 0x333333, transparent: true, opacity: MAP_OPACITY}),
-    GRASS_MATERIAL: new THREE.MeshBasicMaterial({color: 0x006400, transparent: true, opacity: MAP_OPACITY}),
-    WATER_MATERIAL: new THREE.MeshBasicMaterial({color: 0xADD8E6, transparent: true, opacity: MAP_OPACITY}),
-    BUILDING_MATERIAL: new THREE.MeshBasicMaterial({color: 0x000080, transparent: true, opacity: MAP_OPACITY}),
+    SPHERE_MATERIAL: new THREE.MeshPhongMaterial({color: 0xffff00}),
+    ROAD_MATERIAL: new THREE.MeshPhongMaterial({color: 0x333333, transparent: true, opacity: MAP_OPACITY}),
+    GRASS_MATERIAL: new THREE.MeshPhongMaterial({color: 0x006400, transparent: true, opacity: MAP_OPACITY}),
+    WATER_MATERIAL: new THREE.MeshPhongMaterial({color: 0xADD8E6, transparent: true, opacity: MAP_OPACITY}),
+    BUILDING_MATERIAL: new THREE.MeshPhongMaterial({color: 0x000080, transparent: true, opacity: MAP_OPACITY}),
     RED: new THREE.MeshPhongMaterial({ color: 0xff0000 }),
     BLUE: new THREE.MeshPhongMaterial({ color: 0x0000ff }),
 };
+
 
