145fc277716e58ab6625647c6c22b196e8f33634
Alvaro <alvarofreire3@gmail.com>
2023-05-02 04:56:41 +0200

    test 2 animacion por partes

--- materials.js
+++ materials.js
@@ -4,7 +4,13 @@ const MAP_OPACITY = 0.5;
 
 export const materials = {
     SPHERE_MATERIAL: new THREE.MeshPhongMaterial({color: 0xffff00}),
-    ROAD_MATERIAL: new THREE.MeshPhongMaterial({color: 0x333333, transparent: true, opacity: MAP_OPACITY}),
+    ROAD_MATERIAL: new THREE.MeshPhongMaterial({
+        color: 0x333333,
+        transparent: false,
+        polygonOffset: true,
+        polygonOffsetFactor: 1,
+        polygonOffsetUnits: 1
+    }),
     GRASS_MATERIAL: new THREE.MeshPhongMaterial({color: 0x006400, transparent: true, opacity: MAP_OPACITY}),
     WATER_MATERIAL: new THREE.MeshPhongMaterial({color: 0xADD8E6, transparent: true, opacity: MAP_OPACITY}),
     BUILDING_MATERIAL: new THREE.MeshPhongMaterial({color: 0x000080, transparent: true, opacity: MAP_OPACITY}),
